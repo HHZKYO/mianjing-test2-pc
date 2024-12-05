77934b2efcefdab06c1b48da72aaeeb607453601
HHZKYO <1692189552@qq.com>
2024-12-05 16:06:52 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/Login'
+import Layout from '@/views/Layout'
+import Dashboard from '@/views/Dashboard'
+import Article from '@/views/Article'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  { path: '/login', component: Login },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      { path: '/dashboard', component: Dashboard },
+      { path: '/article', component: Article }
+    ]
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>左侧侧边栏</div>
+    <div>
+      <div>头部</div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutIndex'
+}
+</script>
+
+<style>
+
+</style>
