34925c5a7f461b62a86d01b3d4bd8ba5229278f8
HHZKYO <1692189552@qq.com>
2024-12-05 17:47:58 +0800

    完成element-ui的基本校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,12 +2,12 @@
   <div class="login-page">
     <el-card class="login-card">
       <template #header>面经运营后台</template>
-      <el-form>
-        <el-form-item label="用户名：">
-          <el-input placeholder="请输入用户名"></el-input>
+      <el-form :model="form" :rules="rules">
+        <el-form-item label="用户名：" prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
         </el-form-item>
-        <el-form-item label="密码：">
-          <el-input type="password" placeholder="请输入密码"></el-input>
+        <el-form-item label="密码：" prop="password">
+          <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
         </el-form-item>
         <el-form-item class="tc">
           <el-button type="primary">登录</el-button>
@@ -20,7 +20,25 @@
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: ['blur', 'change'] },
+          { min: 5, max: 11, message: '长度为5-11个字符', trigger: ['blur', 'change'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['blur', 'change'] },
+          { min: 5, max: 11, message: '长度为5-11个字符', trigger: ['blur', 'change'] }
+        ]
+      }
+    }
+  }
 }
 </script>
 
