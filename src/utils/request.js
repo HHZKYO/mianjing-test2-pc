8a1ccfec7a7947543df08a4a410c98c2ab976281
HHZKYO <1692189552@qq.com>
2024-12-08 11:10:59 +0800

    响应拦截器，统一处理token过期

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
+import router from '@/router'
 
 // 创建一个新的axios实例
 const request = axios.create({
@@ -10,26 +11,28 @@ const request = axios.create({
 
 // 添加请求拦截器
 request.interceptors.request.use(function (config) {
-  // 在发送请求之前做些什么
   const token = store.state.user.token
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
   return config
 }, function (error) {
-  // 对请求错误做些什么
   return Promise.reject(error)
 })
 
 // 添加响应拦截器
 request.interceptors.response.use(function (response) {
-  // 对响应数据做点什么
   return response.data
 }, function (error) {
   if (error.response) {
-    Message.error(error.response.data.message)
+    if (error.response.status === 401) {
+      Message.error('尊敬的用户，当前登录状态已过期！')
+      store.commit('user/logout')
+      router.push('/login')
+    } else {
+      Message.error(error.response.data.message)
+    }
   }
-  // 对响应错误做点什么
   return Promise.reject(error)
 })
 
